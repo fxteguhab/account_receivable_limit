21e6a0d12492105a473e5359ce5d6f037c9c7ec4
Aldo Bagaskara <aaelsldehoc@gmail.com>
2017-05-16 11:12:57 +0700

    Implements res_partner_receivable_limit_log

--- res_partner.py
+++ res_partner.py
@@ -6,10 +6,31 @@ class res_partner(osv.osv):
 	
 	_columns = {
 		'is_overlimit_enabled': fields.boolean('Is Overlimit Enabled')
-		# Kalau True maka meskipun limit piutang diset tapi tetap boleh overlimit.
-		# 	Note: di view, posisi field ini adalah di bawah credit_limit
 	}
 	
 	_defaults = {
 		'is_overlimit_enabled': False
 	}
+
+
+class res_partner_receivable_limit_log(osv.osv):
+	_name = 'res.partner.receivable.limit.log'
+	
+	_columns = {
+		'partner_id': fields.many2one('res.partner', 'Customer', required=True, ondelete='cascade'),
+		'change_date': fields.datetime('Change Date', required=True),
+		'old_limit': fields.float('Old Limit', required=True),
+		'new_limit': fields.float('New Limit', required=True),
+		'change_by': fields.many2one('res.users', 'Change by')
+	}
+	
+	_defaults = {
+		'change_by': lambda self, cr, uid, context: uid
+	}
+	
+	def name_get(self, cr, uid, ids, context=None):
+		result = []
+		for log in self.browse(cr, uid, ids, context):
+			name = log.change_date + ' / ' + log.partner_id.name
+			result.append((log.id, name))
+		return result
