8888f75d6b1c36c07740b7757ee11455969f3db5
Aldo Bagaskara <aaelsldehoc@gmail.com>
2017-05-16 09:26:52 +0700

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,29 @@
+{
+	'name': 'Account Receivable Limit',
+	'version': '1.0',
+	'author': 'Christyan Juniady and Associates',
+	'maintainer': 'Christyan Juniady and Associates',
+	'category': 'General',
+	'sequence': 21,
+	'website': 'http://www.chjs.biz',
+	'summary': '',
+	'description': """
+		Stock Opname
+	""",
+	'author': 'Christyan Juniady and Associates',
+	'images': [
+	],
+	'depends': ['base','board','web','stock'],
+	'data': [
+	],
+	'demo': [
+	],
+	'test': [
+	],
+	'installable': True,
+	'application': True,
+	'auto_install': False,
+	'qweb': [
+		'static/src/xml/ipc_idss.xml'
+	 ],
+}
