c440bdd95a60ff55e652ff71be24c3c656f38b37
Antonius <antoniusa15a@gmail.com>
2017-05-30 16:07:31 +0700

    save memory field

--- res_config.py
+++ res_config.py
@@ -9,7 +9,7 @@ class sale_order_config_settings(osv.osv_memory):
 	_inherit = 'res.config.settings'
 	
 	_columns = {
-		'limit_sale_order_date': fields.date("Limit Sale Order Date",
+		'default_limit_sale_order_date': fields.date("Limit Sale Order Date", default_model='sale.order',
 			help="This setting is for limit to show Sale Order list. Just show Sale Order with date greater than this setting date."),
 	}
 	
@@ -22,12 +22,12 @@ class sale_order_config_settings(osv.osv_memory):
 			if isinstance(field, fields.related) and fname in values:
 				vals[fname] = values[fname]
 		self.write(cr, uid, [id], vals, context)
-		
+
 		sale_order_obj = self.pool.get('sale.order')
 		sale_order_ids = sale_order_obj.search(cr, uid, [])
-		if values.get('limit_sale_order_date', False):
+		if values.get('default_limit_sale_order_date', False):
 			sale_order_obj.write(cr, uid, sale_order_ids, {
-				'limit_sale_order_date': values['limit_sale_order_date'],
+				'limit_sale_order_date': values['default_limit_sale_order_date'],
 			}, context=context)
 		else:
 			sale_order_obj.write(cr, uid, sale_order_ids, {
